61d9acf7edf487266a0608707cab045fedf4f7f3
Brian Kidney <bkidney@ieee.org>
2011-06-06 16:04:26 -0230

    - Converted stylesheets to compass/scss. - Updated Jekyll config to ignore compass/scss files. - Removed Blueprint standalone install. - Moved to proper separation of content/style.

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css"
-sass_dir = "assets/scss"
+sass_dir = "assets/_scss"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
 
